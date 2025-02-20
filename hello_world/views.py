b765cdd8f9fa46c9d1ba626c30f86e0cfc490741
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-02-20 15:20:35 +0100

    add about app

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
-    retunr HttpResponse("Hello, world!")
+    return HttpResponse("Hello, world!")
