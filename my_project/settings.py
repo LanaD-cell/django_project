4c2b36873076fe7df1c6b54bdf839861b59aa637
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-02-21 07:20:52 +0100

    add heroku as host and debug to false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4@pg36=mczpiozo@0dg-kww(l$kj*$+9%=h3!97oesvtzm1)*b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
